e8934420f2d675477cae911ec071e42156fa72d2
Duy Nguyen <duy210697@gmail.com>
2022-03-07 14:33:27 +0700

    create client for sfu ion

--- index.js
+++ index.js
@@ -12,13 +12,3 @@ http.listen(3000, () => {
 server.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function (socket) {
-    io.sockets.emit('user-joined', { clients:  Object.keys(io.sockets.clients().sockets), count: io.engine.clientsCount, joinedUserId: socket.id});
-    socket.on('signaling', function(data) {
-        io.to(data.toId).emit('signaling', { fromId: socket.id, ...data });
-    });
-    socket.on('disconnect', function() {
-        io.sockets.emit('user-left', socket.id)
-    })
-});
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,31 @@
+const URL = 'ws://localhost:7001/ws';
+
+const localVideo = document.getElementById('localVideo');
+const remotesDiv = document.getElementById("remotes");
+
 const mediaStreamConstraints = {
-    video: true
+    video: true,
+    audio: true,
 };
 const offerOptions = {
     offerToReceiveVideo: 1,
 };
-const localVideo = document.getElementById('localVideo');
 let localStream;
 let localUserId;
 let connections = [];
+let connection;
+let remoteConnection;
+let connectionID;
+
 
 function gotRemoteStream(event, userId) {
 
-    let remoteVideo  = document.createElement('video');
+    let remoteVideo = document.createElement('video');
 
     remoteVideo.setAttribute('data-socket', userId);
-    remoteVideo.srcObject   = event.stream;
-    remoteVideo.autoplay    = true;
-    remoteVideo.muted       = true;
+    remoteVideo.srcObject = event.stream;
+    remoteVideo.autoplay = true;
+    remoteVideo.muted = true;
     remoteVideo.playsinline = true;
     document.querySelector('.videos').appendChild(remoteVideo);
 }
@@ -29,98 +38,126 @@ function gotIceCandidate(fromId, candidate) {
 function startLocalStream() {
     navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
         .then(getUserMediaSuccess)
-        .then(connectSocketToSignaling).catch(handleError);
+        .then(connectWSToSignaling).catch(handleError);
 }
 
-function connectSocketToSignaling() {
-    const socket = io.connect('http://localhost:3000', { secure: true });
-    socket.on('connect', () => {
-        localUserId = socket.id;
-        console.log('localUser', localUserId);
-        socket.on('user-joined', (data) => {
-            const clients = data.clients;
-            const joinedUserId = data.joinedUserId;
-            console.log(joinedUserId, ' joined');
-            if (Array.isArray(clients) && clients.length > 0) {
-                clients.forEach((userId) => {
-                    if (!connections[userId]) {
-                        connections[userId] = new RTCPeerConnection(mediaStreamConstraints);
-                        connections[userId].onicecandidate = () => {
-                            if (event.candidate) {
-                                console.log(socket.id, ' Send candidate to ', userId);
-                                socket.emit('signaling', { type: 'candidate', candidate: event.candidate, toId: userId });
-                            }
-                        };
-                        connections[userId].onaddstream = () => {
-                            gotRemoteStream(event, userId);
-                        };
-                        connections[userId].addStream(localStream);
-                    }
-                });
+async function connectWSToSignaling() {
+    // clientLocal.publish(localStream);
+    const socket = new WebSocket(URL);
 
-                if (data.count >= 2) {
-                    connections[joinedUserId].createOffer(offerOptions).then((description) => {
-                        connections[joinedUserId].setLocalDescription(description).then(() => {
-                            console.log(socket.id, ' Send offer to ', joinedUserId);
-                            socket.emit('signaling', {
-                                toId: joinedUserId,
-                                description: connections[joinedUserId].localDescription,
-                                type: 'sdp'
-                            });
-                        }).catch(handleError);
-                    });
-                }
+    socket.onmessage = async event => {
+        const msg = JSON.parse(event.data);
+        if (typeof msg != 'object') {
+            return;
+        }
+
+        const {
+            params,
+            result,
+            method,
+            id,
+        } = msg;
+
+        console.log(method);
+
+        if (id == connectionID) {
+            console.log('set remote');
+            await connection.setRemoteDescription(result);
+        } else if (method == 'offer') {
+            await remoteConnection.setRemoteDescription(params);
+            const description = await remoteConnection.createAnswer();
+
+            await remoteConnection.setLocalDescription(description);
+
+            sendWS(socket, {
+                method: 'answer',
+                params: {
+                    sid: 'test-room',
+                    desc: description,
+                },
+            });
+        } else if (method == 'trickle') {
+            if (params.target == 0) {
+                connection.addIceCandidate(params.candidate);
+            } else {
+                remoteConnection.addIceCandidate(params.candidate);
             }
-        });
+        }
+    };
 
-        socket.on('user-left', (userId) => {
-            let video = document.querySelector('[data-socket="'+ userId +'"]');
-            video.parentNode.removeChild(video);
-        });
+    socket.onopen = async () => {
+        console.log('websocket connected');
+    };
 
-        socket.on('signaling', (data) => {
-            gotMessageFromSignaling(socket, data);
-        });
-    });
-}
+    connection = new RTCPeerConnection(mediaStreamConstraints);
+    remoteConnection = new RTCPeerConnection(mediaStreamConstraints);
 
-function gotMessageFromSignaling(socket, data) {
-    const fromId = data.fromId;
-    if (fromId !== localUserId) {
-        switch (data.type) {
-            case 'candidate':
-                console.log(socket.id, ' Receive Candidate from ', fromId);
-                if (data.candidate) {
-                    gotIceCandidate(fromId, data.candidate);
-                }
-                break;
+    connection.addStream(localStream);
 
-            case 'sdp':
-                if (data.description) {
-                    console.log(socket.id, ' Receive sdp from ', fromId);
-                    connections[fromId].setRemoteDescription(new RTCSessionDescription(data.description))
-                        .then(() => {
-                            if (data.description.type === 'offer') {
-                                connections[fromId].createAnswer()
-                                    .then((description) => {
-                                        connections[fromId].setLocalDescription(description).then(() => {
-                                            console.log(socket.id, ' Send answer to ', fromId);
-                                            socket.emit('signaling', {
-                                                type: 'sdp',
-                                                toId: fromId,
-                                                description: connections[fromId].localDescription
-                                            });
-                                        });
-                                    })
-                                    .catch(handleError);
-                            }
-                        })
-                        .catch(handleError);
-                }
-                break;
+    const description = await connection.createOffer(offerOptions);
 
+    console.log('set local')
+    await connection.setLocalDescription(description);
+
+    connection.onicecandidate = () => {
+        if (event.candidate) {
+            console.log('send candidate');
+            sendWS(socket, {
+                method: 'trickle',
+                params: {
+                    target: 0,
+                    candidate: event.candidate,
+                },
+            });
+        }
+    };
+
+    remoteConnection.onicecandidate = () => {
+        if (event.candidate) {
+            console.log('send candidate');
+            sendWS(socket, {
+                method: 'trickle',
+                params: {
+                    target: 1,
+                    candidate: event.candidate,
+                },
+            });
+        }
+    };
+
+    remoteConnection.ontrack = (ev) => {
+        const tmp = ev.streams[0];
+        const track = ev.track;
+
+        const stream = tmp;
+
+        console.log("got track", track.id, "for stream", stream.id);
+        if (track.kind === "video") {
+            track.onunmute = () => {
+                const remoteVideo = document.createElement("video");
+                remoteVideo.srcObject = stream;
+                remoteVideo.autoplay = true;
+                remoteVideo.muted = true;
+                remotesDiv.appendChild(remoteVideo);
+
+                track.onremovetrack = () => remotesDiv.removeChild(remoteVideo);
+            };
         }
     }
+
+    connection.oniceconnectionstatechange = (event) => {
+        console.log('connection state change to', event.target.iceConnectionState);
+    };
+
+    console.log('send offer to sfu');
+    connectionID = Date.now();
+    sendWS(socket, {
+        method: 'join',
+        params: {
+            sid: 'test-room',
+            offer: description,
+        },
+    });
 }
 
 function getUserMediaSuccess(mediaStream) {
@@ -131,6 +168,17 @@ function getUserMediaSuccess(mediaStream) {
 function handleError(e) {
     console.log(e);
     alert('Something went wrong');
+}
+
+function sendWS(socket, data, noNeedId) {
+    const payload = Object.assign(data, {
+        jsonrpc: '2.0',
+        // id: connectionID,
+    });
+    if (!noNeedId) {
+        payload.id = connectionID;
+    }
+    socket.send(JSON.stringify(payload));
 }
 
 startLocalStream();
